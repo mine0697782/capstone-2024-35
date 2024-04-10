0f968b60c8ba3404e7ae90c41c20514d8829ead0
max <inews2432@gmail.com>
2024-04-10 23:17:58 +0900

    경로 수정

--- src/app.js
+++ src/app.js
@@ -11,24 +11,30 @@ const ejsMate = require("ejs-mate")
 
 //라우팅
 const indexRouter = require('./routes/indexRoutes');
-const mainRouter = require("./routes/user");
-
-
+const loginRouter = require('./routes/user');
 //앱 세팅
 app.set('views', './views');
-// app.set("views", "./login/views");
 app.engine('ejs', ejsMate)
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 app.use(cors())
+app.use(express.json());
 
-app.use('/', indexRouter);
+app.use('/', loginRouter);
+app.use('/api/v1', indexRouter);
 // app.use("/", home); //use -> 미들 웨어를 등록해주는 메서드.
 
 // app.listen(PORT, () => {
 //     console.log(`Server is running on http://localhost:${PORT}`);
 //   });
 // module.exports = app;
+app.get("/", (req, res) => {
+  res.render("login")
+})
+
+app.get("/register", (req, res) =>{
+  res.render("register")
+})
 
 const port = process.env.PORT || 3000;
 
