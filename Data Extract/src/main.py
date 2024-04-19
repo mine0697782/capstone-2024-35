743602f22e85c2868315e19624c1f7ed6f4f9021
Moonsun <38421491+mmoossun@users.noreply.github.com>
2024-04-19 16:54:31 +0900

    Update main.py

--- Data Extract/src/main.py
+++ Data Extract/src/main.py
@@ -5,7 +5,7 @@ from src.data_processing import find_career_status, find_phone_number, extract_a
 #device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 
 model, tokenizer = load_model_and_tokenizer()
-id2tag = {id: tag for tag, id in enumerate(tag_list)}  # tag_list는 사전에 정의되어 있어야 합니다.
+id2tag = {id: tag for tag, id in enumerate(tag_list)}
 
 text = "25/ 김준호 /서초구 거주/경력:유/전화번호:010-0000-0000"
 predicted_entities = predict_entities(text, model, tokenizer, id2tag)
