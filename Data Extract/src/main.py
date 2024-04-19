8f1bf5217fb760471a071e734be60f4b1577c37a
Moonsun <38421491+mmoossun@users.noreply.github.com>
2024-04-19 16:56:15 +0900

    Update main.py

--- Data Extract/src/main.py
+++ Data Extract/src/main.py
@@ -5,7 +5,14 @@ from src.data_processing import find_career_status, find_phone_number, extract_a
 #device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 
 model, tokenizer = load_model_and_tokenizer()
-id2tag = {id: tag for tag, id in enumerate(tag_list)}
+# KLUE NER 데이터셋 로드
+dataset = load_dataset("klue", "ner")
+# 태그 리스트 확인
+tag_list = dataset['train'].features['ner_tags'].feature.names
+# tag2id 및 id2tag 사전 생성
+tag2id = {tag: id for id, tag in enumerate(tag_list)}
+id2tag = {id: tag for tag, id in tag2id.items()}
+
 
 text = "25/ 김준호 /서초구 거주/경력:유/전화번호:010-0000-0000"
 predicted_entities = predict_entities(text, model, tokenizer, id2tag)
