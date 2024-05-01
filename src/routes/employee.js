dae9887a9b931e0104c093262d5a072f6051f9b9
max <inews2432@gmail.com>
2024-05-01 16:49:11 +0900

    각 컨트롤러에 모델, 몽구스 연결

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -4,7 +4,9 @@ const { isLoggedIn } = require('../middleware/checkAuth');
 const employeeController = require('../controllers/employeeController');
 
 router.get('/employee', isLoggedIn, employeeController.employee);
+
 router.get('/addemployee', isLoggedIn, employeeController.addEmployee);
+router.post('/addemployee', isLoggedIn, employeeController.postEmployee);
 
 
 
