c8812db5350770f93aaf7dc570832f3b9c15f882
Poppy <jmj@kookmin.ac.kr>
2024-05-03 00:38:41 +0900

    Career (경력) 모델 추가

---
+++ src/models/Career.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const careerSchema = new Schema({
+	employee: {
+    	type: Schema.Types.ObjectId,
+        ref: 'Employee'
+    },
+    worksite: {
+    	type: Schema.Types.ObjectId,
+        ref: 'Worksite'
+    }
+});
+
+module.exports = mongoose.model('Career', careerSchema)
