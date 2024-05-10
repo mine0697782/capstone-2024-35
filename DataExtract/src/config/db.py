df114765c68ca8a042228c0dbafc4e25aa785259
mmoossun <song980805@gmail.com>
2024-05-10 16:31:08 +0900

    code update

--- DataExtract/src/config/db.py
+++ DataExtract/src/config/db.py
@@ -13,3 +13,6 @@ def connect_db():
     client = MongoClient(mongodb_uri)
     db = client['Authusers']  # 데이터베이스 이름을 여기에서 변경 가능
     return db
+def get_collection(collection_name):
+    db = connect_db()
+    return db[collection_name]
--- DataExtract/src/main.py
+++ DataExtract/src/main.py
@@ -3,11 +3,12 @@
 from model import load_model_and_tokenizer, predict_entities
 from data_processing import find_career_status, find_phone_number, extract_and_combine_entities
 from datasets import load_dataset
-from config.db import connect_db
+from config.db import connect_db, get_collection
+from employee import Employee, EmployeeRepository
 
 # MongoDB 데이터베이스 연결
 db = connect_db()
-collection = db['ExtractedEntities']  # 원하는 컬렉션 이름을 지정
+collection = get_collection('ExtractedEntities')  # 원하는 컬렉션 이름을 지정
 
 # KLUE NER 데이터셋 로드
 dataset = load_dataset("klue", "ner")
@@ -25,8 +26,23 @@ text = "25/ 김준호 /서초구 거주/경력:유/전화번호:010-0000-0000"
 predicted_entities = predict_entities(text, model, tokenizer, id2tag)
 entities_combined = extract_and_combine_entities(predicted_entities)
 entities_combined["career"] = find_career_status(text)
-entities_combined["phone_number"] = find_phone_number(text)
+entities_combined["phonenumber"] = find_phone_number(text)
+entities_combined["sex"] = "남"
+entities_combined["RRN"] = "000000-0000000"
 
+user_id = '609b8b8f8e4f5b88f8e8e8e8'
+
+new_employee = Employee(
+    user=user_id,
+    name=entities_combined["name"],
+    sex=entities_combined["sex"],
+    local=entities_combined["local"],
+    rrn=entities_combined["RRN"],
+    phonenumber=entities_combined["phonenumber"]
+    )
 # 데이터 MongoDB에 저장
-insert_result = collection.insert_one(entities_combined)
-print(f"삽입된 문서 ID: {insert_result.inserted_id}")
+employee_repo = EmployeeRepository()
+employee_repo.insert(new_employee)
+
+for employee in employee_repo.find_all():
+    print(employee)
